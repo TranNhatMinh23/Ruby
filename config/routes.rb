2fde37a92fbacbb6d0e7e8ceb74b9d0d70b731e2
Tran Nhat Minh <trannhatminh23082001@gmail.com>
2023-04-10 14:00:28 +0700

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope 'auth' do
-    devise_for :parents
-    devise_for :teachers
-    devise_for :students
-    devise_for :users
-  end
-
-  resources :users
-  resources :parents
-  resources :teachers
-  resources :exam_categories
-  resources :grades
-
-  resources :students
-  resources :exams
-  resources :courses
-  resources :attendances
-  resources :classrooms
-
-  root 'dashboard#index'
-  
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,172 +2,59 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923025443) do
-
-  create_table "attendances", force: :cascade do |t|
-    t.integer "student_id"
-    t.date "start_date"
-    t.date "end_date"
-    t.string "status"
-    t.string "remark"
+ActiveRecord::Schema[7.1].define(version: 2022_12_30_120451) do
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["student_id"], name: "index_attendances_on_student_id"
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table "classrooms", force: :cascade do |t|
-    t.string "name"
-    t.string "year"
-    t.integer "grade_id"
-    t.string "section"
-    t.string "status"
-    t.string "remarks"
-    t.integer "teacher_id"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["grade_id"], name: "index_classrooms_on_grade_id"
-    t.index ["teacher_id"], name: "index_classrooms_on_teacher_id"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "courses", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "grade_id"
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["grade_id"], name: "index_courses_on_grade_id"
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "exam_categories", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "exams", force: :cascade do |t|
-    t.integer "exam_category_id"
-    t.string "name"
-    t.date "start_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["exam_category_id"], name: "index_exams_on_exam_category_id"
-  end
-
-  create_table "grades", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "parents", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
-    t.date "dob"
-    t.string "phone"
-    t.text "address"
-    t.float "lat"
-    t.float "long"
-    t.string "last_login_ip", default: "127.0.0.1"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "gender"
-    t.string "image"
-    t.index ["email"], name: "index_parents_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_parents_on_reset_password_token", unique: true
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
-    t.date "dob"
-    t.string "phone"
-    t.text "address"
-    t.float "lat"
-    t.float "long"
-    t.string "last_login_ip"
-    t.integer "parent_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "gender"
-    t.string "image"
-    t.index ["email"], name: "index_students_on_email", unique: true
-    t.index ["parent_id"], name: "index_students_on_parent_id"
-    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
-    t.date "dob"
-    t.string "gender"
-    t.string "image"
-    t.string "phone"
-    t.text "address"
-    t.float "lat"
-    t.float "long"
-    t.string "last_login_ip", default: "127.0.0.1"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_teachers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "name"
-    t.string "gender"
-    t.string "phone"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
